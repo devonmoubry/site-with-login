6b34d4f5c995f401ed03054b8a989146fd57fc0c
Devon Moubry <devon@moubry.com>
2017-08-16 21:46:05 -0500

    add session function

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mustacheExpress = require('mustache-express');
 const session = require('express-session');
+
 const app = express();
+
+const users = [{username: 'dumbledore', password: 'sherbert-lemon'}];
 
 // Register '.mustache' extension with The Mustache Express
 app.engine('mustache', mustacheExpress());
@@ -10,12 +13,22 @@ app.engine('mustache', mustacheExpress());
 app.set('view engine', 'mustache');
 app.set('views', __dirname + '/views');
 
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Hello World!');
 })
 
 app.listen(3000, function () {
-  console.log('App listening on port 3000!')
-})
+  console.log('App listening on port 3000!');
+});
