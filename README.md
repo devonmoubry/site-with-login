30222ad0acc0cef1da646c0286534cbba243be8a
Devon Moubry <devon@moubry.com>
2017-08-16 21:01:55 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Site with Login using express-session
+
+- Node.js
+- Express.js
+- Mustache
+- mustache-express
+- express-session
+
+Build Steps:
+- `mkdir <project-name>`
+- `cd <project-name`
+- `gitignore node`
+- `npm init --yes`
+- `npm install`
+- `npm install express mustache mustache-express express-session body-parser --save`
+- `touch README.md`
+- `touch app.js`
+- `mkdir views`
+- `cd views`
+- `touch index.mustache`
+- `touch login.mustache`
---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mustacheExpress = require('mustache-express');
+const session = require('express-session');
+const app = express();
+
+// Register '.mustache' extension with The Mustache Express
+app.engine('mustache', mustacheExpress());
+
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('App listening on port 3000!')
+})
